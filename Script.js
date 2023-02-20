0233c0b4f18672e16ef87e7406409864912b1500
Gael Adam <gaelswim@hotmail.com>
2023-02-20 21:01:32 +0100

    twenty-fourth commit

--- Script.js
+++ Script.js
@@ -1,19 +1,36 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
 $(".burger-menu ").on("click",".bar",function(){
-  
-    
     $(".menu").slideToggle();
     $(".bar").toggleClass('change');
     $(".menu li").slideRight();
-  
-   });
 
+     });
 
-   $("#connection").on("click",function(){
+/*$(document).ready(function () {
+    $("#form").validate({
+        rules: {
+        email: {
+            required: true,
+             email: true         
+            },
+            
+        password: {
+                required: true,
+                 minlength:8  
+                },
+                messages : {
+                    email: {
+                    email: "The email should be in the format: abc@domain.tld"
+                    
+                        },
+                        
+                    }
+            }
+         
+              
+    });
+    });*/
+
     
-
-   });
-
-  /* $(document).ready(function () {
-    
-    
-   });*/
